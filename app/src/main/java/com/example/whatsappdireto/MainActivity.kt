eb5d2c2e312df40b1f1608def8671411e99ea480
caio_ <caiooalexandre94@gmail.com>
2024-06-04 15:22:44 -0500

    configuracao do menu bottom, inserido mascara de texto

--- app/src/main/java/com/example/whatsappdireto/MainActivity.kt
+++ app/src/main/java/com/example/whatsappdireto/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.whatsappdireto
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -16,11 +19,30 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
+        //inicializar navegação Menu Bottom
         navController = Navigation.findNavController(this, R.id.fragmentContainerView)
         setupWithNavController(binding.bottomNavigation,navController)
 
+        configToolbar()
+
+
+
+
+
+
+    }
+
+    private fun configToolbar() {
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(Color.WHITE)
+        if (Build.VERSION.SDK_INT >= 24) {
+            supportActionBar?.title = Html.fromHtml("<b>ZapDireto</b>", 0)
+        } else {
+            supportActionBar?.title = Html.fromHtml("<b>ZapDireto</b>")
+        }
     }
 }
--- app/src/main/java/com/example/whatsappdireto/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/whatsappdireto/fragments/HomeFragment.kt
@@ -6,18 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.whatsappdireto.R
+import com.example.whatsappdireto.databinding.FragmentHomeBinding
+import com.example.whatsappdireto.masks.MaskEditTextChangedListener
 
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding
+        get() = _binding!!
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+       iniciarMascara()
+    }
+
+    private fun iniciarMascara() {
+        val editText = binding.edtPhone
+        val maskPhone = MaskEditTextChangedListener(
+            "(##) #####-####",
+            editText
+        )
+        editText.addTextChangedListener(maskPhone)
+
     }
 
 
